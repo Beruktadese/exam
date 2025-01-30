ca860a64934ae38ef0af8d7e29b568674b407294
Beruktadese <buramelkam@gmail.com>
2025-01-30 20:41:30 +0300

    python

---
+++ python practice/main.py
@@ -0,0 +1,20 @@
+# to accept three number form user and to find the average of the number
+
+'''x=int(input("enter the number"))
+y=int(input("enter the number"))
+z=int(input("enter the number"))
+a=x+y+z
+b=a/3
+print(b)
+'''
+
+# to find the persentage of female and male 
+'''
+F=int(input("enter the number of female"))   # f is stands for female
+M=int(input("enter the number of male"))   #M is stands for male
+k=F+M
+L=F/k
+print("the persentage of female is ",L)
+P=M/k
+print("the persentage of male is",P)
+'''
